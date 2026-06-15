851dc5384258edbbd78ad2d26c6b6a0157bb212a
Sagar Rathod <sagar.rathod.tech@gmail.com>
2026-06-15 09:12:26 +0530

    New update on func

---
+++ fucns/largest_num.py
@@ -0,0 +1,4 @@
+def largest_num(numbers):
+    return min(numbers)
+result = largest_num([1, 4, 6, 7, 2],)
+print("The largest number is: ",result)
---
+++ fucns/reverse_string.py
@@ -0,0 +1,4 @@
+def reverse_string(name):
+    return name[::-1]
+result = reverse_string("sagar")
+print("Reverse String: ",result)
